c5d941a3d748c6cf59e83ae7b541479e0ad095b6
carloshss97@gmail.com <carloshss97@gmail.com>
2025-06-03 11:13:40 -0300

    feat: adding the UpdateBalance method

--- internal/gateway/account.go
+++ internal/gateway/account.go
@@ -5,5 +5,6 @@ import "github.com/carloshss0/walletcore/internal/entity"
 type AccountGateway interface {
 	Save(account *entity.Account) error
 	FindByID(id string) (*entity.Account, error)
+	UpdateBalance(account *entity.Account) error
 }
 
