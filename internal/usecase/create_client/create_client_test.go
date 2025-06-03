1454d4a50a4866315db6b6c7162acb70ff35e5a9
carloshss97@gmail.com <carloshss97@gmail.com>
2025-06-03 11:14:44 -0300

    fix: refactoring the tests of usescases to use the mocks in the mocks folder

--- internal/usecase/create_client/create_client_test.go
+++ internal/usecase/create_client/create_client_test.go
@@ -3,28 +3,15 @@ package createclient
 import (
 	"testing"
 
-	"github.com/carloshss0/walletcore/internal/entity"
+	"github.com/carloshss0/walletcore/internal/usecase/mocks"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
 
-type ClientGatewayMock struct {
-	mock.Mock
-}
-
-func (m *ClientGatewayMock) Save(client *entity.Client) error {
-	args := m.Called(client)
-	return args.Error(0)
-}
-
-func (m *ClientGatewayMock) Get(id string) (*entity.Client, error) {
-	args := m.Called(id)
-	return args.Get(0).(*entity.Client), args.Error(1)
-}
 
 
 func TestCreateClientUseCaseExecute(t *testing.T) {
-	m := &ClientGatewayMock{}
+	m := &mocks.ClientGatewayMock{}
 	m.On("Save", mock.Anything).Return(nil)
 
 	uc := NewCreateClientUseCase(m)
--- internal/usecase/create_transaction/create_transaction_test.go
+++ internal/usecase/create_transaction/create_transaction_test.go
@@ -1,38 +1,16 @@
 package createtransaction
 
 import (
+	"context"
 	"testing"
 
 	"github.com/carloshss0/walletcore/internal/entity"
 	"github.com/carloshss0/walletcore/internal/event"
+	"github.com/carloshss0/walletcore/internal/usecase/mocks"
 	"github.com/carloshss0/walletcore/pkg/events"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 )
-
-type TransactionGatewayMock struct {
-	mock.Mock
-}
-
-func (m *TransactionGatewayMock) Create(transaction *entity.Transaction) error {
-	args := m.Called(transaction)
-	return args.Error(0)
-}
-
-type AccountGatewayMock struct {
-	mock.Mock
-}
-
-func (m *AccountGatewayMock) Save(account *entity.Account) error {
-	args := m.Called(account)
-	return args.Error(0)
-}
-
-func (m *AccountGatewayMock) FindByID(id string) (*entity.Account, error) {
-	args := m.Called(id)
-	return args.Get(0).(*entity.Account), args.Error(1)
-}
-
 
 
 func TestCreateTransactionUseCaseExecute(t *testing.T) {
@@ -44,12 +22,8 @@ func TestCreateTransactionUseCaseExecute(t *testing.T) {
 	account2 := entity.NewAccount(client2)
 	account2.Credit(1000)
 
-	mockAccount := &AccountGatewayMock{}
-	mockAccount.On("FindByID", account1.ID).Return(account1, nil)
-	mockAccount.On("FindByID", account2.ID).Return(account2, nil)
-
-	mockTransaction := &TransactionGatewayMock{}
-	mockTransaction.On("Create", mock.Anything).Return(nil)
+	mockUow := &mocks.UowMock{}
+	mockUow.On("Do", mock.Anything, mock.Anything).Return(nil)
 
 	inputDto := CreateTransactionInputDTO{
 		AccountIDFrom: account1.ID,
@@ -59,18 +33,18 @@ func TestCreateTransactionUseCaseExecute(t *testing.T) {
 
 	dispatcher := events.NewEventDispatcher()
 	event := event.NewTransactionCreated()
+	ctx := context.Background()
 
-	uc := NewCreateTransactionUseCase(mockTransaction, mockAccount, dispatcher, event)
+	uc := NewCreateTransactionUseCase(mockUow, dispatcher, event)
 
-	output, err := uc.Execute(inputDto)
+	output, err := uc.Execute(ctx, inputDto)
 
 	assert.Nil(t, err)
 	assert.NotNil(t, output)
+	mockUow.AssertExpectations(t)
+	mockUow.AssertNotCalled(t, "Do", 1)
 
-	mockAccount.AssertExpectations(t)
-	mockTransaction.AssertExpectations(t)
-	mockAccount.AssertNumberOfCalls(t, "FindByID", 2)
-	mockTransaction.AssertNumberOfCalls(t, "Create", 1)
+
 
 	
 
