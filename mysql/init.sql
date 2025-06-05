2917554d277d7fdcde95824631c84e97ec2cf325
carloshss97@gmail.com <carloshss97@gmail.com>
2025-06-04 22:35:19 -0300

    feat: creating sql script to create tables if it does not exists

---
+++ mysql/init.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS wallet;
+
+USE wallet;
+
+CREATE TABLE IF NOT EXISTS clients (
+    id varchar(255),
+    name varchar(255),
+    email varchar(255),
+    created_at date
+);
+
+CREATE TABLE IF NOT EXISTS accounts (
+    id varchar(255),
+    client_id varchar(255),
+    balance float,
+    created_at date
+)
+
+CREATE TABLE IF NOT EXISTS transactions (
+    id varchar(255),
+    account_id_from varchar(255),
+    account_id_to varchar(255),
+    amount float,
+    created_at date
+)
